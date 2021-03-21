6bf93fa0957fce9a8b28c2a0f83241f34b6fe21c
mohit9090 <66547281+mohit9090@users.noreply.github.com>
2021-03-22 01:20:05 +0530

    update usp section

--- js/slide-nav-toggle.js
+++ js/slide-nav-toggle.js
@@ -6,10 +6,10 @@ const slideNavBtn =  document.getElementsByClassName("slide-navigation-btn")[0];
 function opennav() {
 	slideNavigation.style.left = "0";
 	overlayEffect.style.width = "100%";
-	slideNavBtn.setAttribute("onclick","closenav()");
+	// slideNavBtn.setAttribute("onclick","closenav()");
 }
 function closenav() {
 	slideNavigation.style.left = "-100%";
 	overlayEffect.style.width = "0";
-	slideNavBtn.setAttribute("onclick","opennav()");
+	// slideNavBtn.setAttribute("onclick","opennav()");
 }
