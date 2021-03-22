18cf43d94913794407e80475ab1ce295c5ea9bf6
mohit9090 <66547281+mohit9090@users.noreply.github.com>
2021-03-23 01:19:32 +0530

    stop scrolling of body when slidenav is opened

--- js/slide-nav-toggle.js
+++ js/slide-nav-toggle.js
@@ -6,10 +6,12 @@ const slideNavBtn =  document.getElementsByClassName("slide-navigation-btn")[0];
 function opennav() {
 	slideNavigation.style.left = "0";
 	overlayEffect.style.width = "100%";
-	// slideNavBtn.setAttribute("onclick","closenav()");
+	//turn off the body scrolling effect
+	document.body.style.overflowY="hidden";
 }
 function closenav() {
 	slideNavigation.style.left = "-100%";
 	overlayEffect.style.width = "0";
-	// slideNavBtn.setAttribute("onclick","opennav()");
+	//turn on the body scrolling effect
+	document.body.style.overflowY="auto";
 }
