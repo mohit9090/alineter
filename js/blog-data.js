cf79e5fc89daa1f8f1a36667ed8add7cd8a0ed55
mohit9090 <66547281+mohit9090@users.noreply.github.com>
2021-04-07 01:01:13 +0530

    Separate space for blogs data

---
+++ js/blog-data.js
@@ -0,0 +1,122 @@
+blogs = [
+	{
+		blog_id : "23647",
+		blog_detail : {
+			mainImg : "images/blog-img--1.webp", 
+			title : "0.1 seconds is all it takes",
+			short_description : "What are the things people notice first to form a quick opinion on you? Here's your answer",
+			url : "blog/0-1-seconds-is-all-it-takes",
+			blog_category : [
+				"clothing", 
+				"men's fashion", 
+				"lifestyle"
+			]
+		},
+		blog_author : {
+			name : "Mohit Kumar",
+			username : "mohitkumar3762",
+			pfImg : "images/mohit_300.png",
+			profile_url : "profile/mohitkumar3762"
+		},
+		blog_date : {
+			date : "22",
+			month : "Mar",
+			year : "2021"
+		},
+		blog_reviews : {
+			views : "86",
+			comments : "3",
+			likes : "15"
+		}
+	},
+
+	{
+		blog_id : "974766",
+		blog_detail : {
+			mainImg : "images/men-fashion-1.jpg", 
+			title : "Backed By Science",
+			short_description : "Humans have a thing called a learning bias. No matter how wise a saying is, we are much more apt to accept it as true if we trust the source.",
+			url : "blog/Backed-By-Science",
+			blog_category : [ 
+				"research backed"
+			]
+		},
+		blog_author : {
+			name : "Rohit Kumar",
+			username : "rohitkumar_292",
+			pfImg : "images/model_pic_1859__white_tee_park__jpeg--20.jpg",
+			profile_url : "profile/rohitkumar_292"
+		},
+		blog_date : {
+			date : "18",
+			month : "Mar",
+			year : "2021"
+		},
+		blog_reviews : {
+			views : "6",
+			comments : "23",
+			likes : "5"
+		}
+	},
+
+	{
+		blog_id : "917329",
+		blog_detail : {
+			mainImg : "images/men-fashion-2.jpg", 
+			title : "Let Me List Them Out For You",
+			short_description : "For some reason, we like list posts. They appeal to a wide audience and inspire a lot of clicks compared to other types of articles",
+			url : "blog/Let-Me-List-Them-Out-For-You",
+			blog_category : [
+				"clothing",
+				"research backed"
+			]
+		},
+		blog_author : {
+			name : "Sanmay Biswal",
+			username : "sanmayBiswal00",
+			pfImg : "images/pratyush_pic_no_1516_jpeg-30.jpg",
+			profile_url : "profile/sanmayBiswal00"
+		},
+		blog_date : {
+			date : "2",
+			month : "Feb",
+			year : "2021"
+		},
+		blog_reviews : {
+			views : "186",
+			comments : "32",
+			likes : "150"
+		}
+	},
+
+	{
+		blog_id : "123444",
+		blog_detail : {
+			mainImg : "images/men-fashion-3.jpg", 
+			title : "Everyone Loves Competition",
+			short_description : "This is a powerful title option. It allows you to replace third-party, uncontrollable reviews of your product or service with reviews you can control.",
+			url : "blog/Everyone-Loves-Competition",
+			blog_category : [
+				"clothing"
+			]
+		},
+		blog_author : {
+			name : "MD Pervez",
+			username : "PervezKhan024",
+			pfImg : "images/rahul_pic_no_1503_jpeg-30.jpg",
+			profile_url : "profile/PervezKhan024"
+		},
+		blog_date : {
+			date : "22",
+			month : "Dec",
+			year : "2020"
+		},
+		blog_reviews : {
+			views : "56",
+			comments : "35",
+			likes : "5"
+		}
+	},
+];
+
+jsonBlogs = JSON.stringify(blogs);
