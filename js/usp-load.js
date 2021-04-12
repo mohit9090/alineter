fee72b86d57cfaccbda6efd50137b796f784bdd3
mohit9090 <66547281+mohit9090@users.noreply.github.com>
2021-04-12 13:28:38 +0530

    add animation on usp-icons on click, add mobile device detector using js

--- js/usp-load.js
+++ js/usp-load.js
@@ -1,4 +1,6 @@
   
+  const uspContainer = document.getElementById("usp-wrapper")
+ 
   //jsonUSP is derived from js/usp-data.js
   const usp_icons = JSON.parse(jsonUSP)
 
@@ -33,20 +35,118 @@
     }
   ]
 
-  uspList.map( usp => {
-    document.getElementById("usp-wrapper").innerHTML += `
-      <div class="card text-center usp-box reveal__from-bottom mx-auto">
-        <div class="card-body">
-          <div class="my-auto">
-            <div class="mx-auto icon-filler">
-              <!--icon-->
-              ${usp.icon}
-            </div><br>
-            <small class="txt-primary usp-tag">${usp.tag}</small>
-          </div>
+
+
+function mapUSP(uspList) {
+  let usp_div = "";
+
+  uspList.map( (usp) => {
+    usp_div += `<div class="card text-center usp-box reveal__from-bottom mx-auto" onclick="animateIcon(this)">
+      <div class="card-body">
+        <div class="my-auto">
+          <div class="mx-auto icon-filler">
+            <!--icon-->
+            ${usp.icon}
+          </div><br>
+          <small class="txt-primary usp-tag">${usp.tag}</small>
         </div>
       </div>
-    `
+    </div>`;
   });
 
+  return usp_div;
+};
+
+function classContains(elem, className) {
+  return elem.classList.contains(className);
+}
+
+function removeClass(elem, __classList) {
+  /*
+    Remove classes from desired Element(elem)
+    __classList is passed as an ARRAY
+  */
+  elem.classList.remove(__classList);
+}
+
+function addClass(elem, __classList) {
+  /*
+    Add classes from desired Element(elem)
+    __classList is passed as an ARRAY
+  */
+  elem.classList.add(__classList);
+}
+
+function addMobile__Class(elemList) {
+  /*
+    Add mobile-device class to Specified Elements List
+  */
+  elemList.map( elem => {
+    addClass(elem, ["mobile-device"]);
+  });
+};
+
+function removeMobile__Class(elemList) {
+  /*
+    Remove mobile-device class to Specified Elements List
+    if it contains mobile-device class
+  */
+  elemList.map( elem => {
+    if(classContains(elem, "mobile-device")) {
+      removeClass(elem, ["mobile-device"]);
+    }
+  })
+};
+
+function getUSP_DIV() {
+  let uspIconsElem = document.querySelectorAll(".usp-box");
+  
+  /* 
+    Opening Object into Array List using Spread Operator and returning it
+  */
+  return [...uspIconsElem];
+}
+
+function animateIcon(icon) {
+  /*
+    Animate Icons on clicking them and remove animation after some specific interval
+  */
+  let uspIcons = getUSP_DIV();
+  
+  uspIcons.map(u_icon => {
+    if(classContains(u_icon, "clicked")) {
+      removeClass(u_icon, ["clicked"]);
+    }
+  });
+
+  addClass(icon, ["clicked"])
+  setTimeout(function() {
+    removeClass(icon, ["clicked"]);
+  }, 700);
+};  
+
+
+function onSuccessAddClass() {
+  /* 
+    Add class mobile-device to usp-icons if the device is mobile
+    This func will encounter both on load and on resize
+  */
+  let uspIcons = getUSP_DIV();
+
+  let isMobile = detectMob();
+  isMobile ? addMobile__Class(uspIcons) : removeMobile__Class(uspIcons);
+};
+
+function constructUSP() {
+  let usp_html = "";
+
+  usp_html = mapUSP(uspList);
+  uspContainer.innerHTML = usp_html;
+
+  const events = ["load", "resize"];
+  events.map(event => {
+    window.addEventListener(event, onSuccessAddClass)
+  });
+};
+constructUSP();
 
